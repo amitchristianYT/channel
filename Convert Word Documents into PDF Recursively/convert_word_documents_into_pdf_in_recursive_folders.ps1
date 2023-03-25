1a24a47f49e2aae76a9023a70437ac0684358e0b
amitchristianYT <102202463+amitchristianYT@users.noreply.github.com>
2023-03-25 14:05:39 -0500

    Update convert_word_documents_into_pdf_in_recursive_folders.ps1

--- Convert Word Documents into PDF Recursively/convert_word_documents_into_pdf_in_recursive_folders.ps1
+++ Convert Word Documents into PDF Recursively/convert_word_documents_into_pdf_in_recursive_folders.ps1
@@ -2,13 +2,31 @@
 #
 # 2023-02-26
 #
-# https://www.youtube.com/amitchristian
+# Watch the full tutorial demo of how to run this script here.
+# https://www.youtube.com/watch?v=C6hzcyCZx4E
 # Please subscribe to my Youtube Channel for more tips and tricks
 #
 # Enable Powershell script execution using this command
 #   set-executionpolicy remotesigned
 #
-# Make Sure that you set your folder path in first line
+# STEPS
+# 1. Download this script by copying and pasting or by clicking 'Raw' button on
+#    top right and then right click and save as <Anyname>.ps1.
+#    (IMP: Make sure that the extension of the saved file is in fact, ps1 and 
+#    not .ps1.txt, which essentially is a text file)
+# 2. Copy the ps1 file, to the root folder location where all your pptx files
+#    are located.
+# 3. Enable powersheel script execution using following command.
+#      set-executionpolicy remotesigned
+# 4. Make Sure that you set your folder path in first line of the script below.
+# 5. Call the ps1 function from powershell, as shown in the video.
+# 
+# TROUBLESHOOTING
+# - Please add a comment if you are getting errors running this script here.
+#   https://www.youtube.com/watch?v=C6hzcyCZx4E
+#
+# Thank you.
+# Cheers.
 #
 #################################################################
  $Folders = Get-ChildItem C:\Users\<Your-UserName>\desktop\test_doc -Directory
